90c3e469ef98e741d1f0f7e6ec46264823a9cf62
FangAolin <fangaolin@foxmail.com>
2020-08-13 18:07:47 +0800

    feat:单链表删除

--- link.c
+++ link.c
@@ -5,7 +5,7 @@ typedef struct node
     int pos;
     int data;
     struct node *next;
-} *llist, *lnode, node;
+} * llist, *lnode, node;
 
 void insert(llist list, int data, int pos)
 {
@@ -33,7 +33,37 @@ void insert(llist list, int data, int pos)
 
 void delete (llist list, int pos)
 {
+    // 如果找到该节点
     if (list->pos == pos)
     {
+        // 链表指针临时存储
+        llist tempNode = list;
+        // 链表移到下个节点
+        list = list->next;
+        // 释放空间
+        free(tempNode);
+    }
+    else
+    {
+        // 1. 移到下一个，这里tempList比currentNode慢一个节点
+        lnode tempList = list;
+        lnode currentNode = tempList->next;
+
+        // 2. 开始遍历
+        while (tempList->next != NULL)
+        {
+            if (currentNode->pos = pos)
+            {
+                tempList->next = currentNode->next;
+                free(currentNode);
+                break;
+            }
+            else
+            {
+                // 向前移一个节点
+                tempList = currentNode;
+                currentNode = currentNode->next;
+            }
+        }
     }
 }
